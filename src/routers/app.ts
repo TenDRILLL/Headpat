41735b79e196834e3cfa2137eec68a79d437e9f2
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2024-03-04 19:43:39 +0200

    fix<>: remove secrets

--- src/routers/app.ts
+++ src/routers/app.ts
@@ -8,7 +8,7 @@ const appRouter = Router();
 
 appRouter.get("/", (req, res)=>{
     if(!req.cookies.auth) return res.redirect("/login");
-    jwtVerify(req.cookies.auth, new TextEncoder().encode(process.env.JWT_SECRET as string ?? "COOLSECRET")).then(async jwtData => {
+    jwtVerify(req.cookies.auth, new TextEncoder().encode(process.env.JWT_SECRET as string)).then(async jwtData => {
         const payload = jwtData.payload;
         if(payload.iss !== "urn:Headpat:axiom" || payload.aud !== "urn:Headpat:users") return res.clearCookie("auth").redirect("/");
         const user = await getUser(payload.id);
