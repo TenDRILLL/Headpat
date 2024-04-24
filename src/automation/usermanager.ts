b567e9ccc866bb3f180639363408d4f4587cdedc
dip-land <82669765+dip-land@users.noreply.github.com>
2024-04-23 20:07:44 -0500

    Update usermanager.ts

--- src/automation/usermanager.ts
+++ src/automation/usermanager.ts
@@ -31,6 +31,7 @@ const createUser = async (id): Promise<User> => {
             ID: id,
             username: "Nya",
             role: "MEMBER",
+            discriminator: await findFreeDiscriminator("Nya"),
             createdAt: Date.now().toString(),
             servers: ["0"]
         };
@@ -42,6 +43,18 @@ const createUser = async (id): Promise<User> => {
         res(user);
     });
 };
+
+const findFreeDiscriminator = (name) => {
+    return new Promise(async res => {
+        let newDisc = Math.floor(1000 + Math.random() * 9000);
+        for await (const [key, value] of rawDatabase("users")!.iterator()) {
+            if(value.name === name){
+                if(value.discriminator === newDisc) return res(findFreeDiscriminator(name));
+            }
+        }
+        return res(newDisc);
+    });
+}
 
 export {
     getUser,
